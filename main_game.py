c5f93160ffd3e821918d1d7380bf502a50305a92
daniel.utikal@seznam.cz <daniel.utikal@seznam.cz>
2025-05-11 16:15:48 +0200

    Finished NGS3

--- main_game.py
+++ main_game.py
@@ -6,12 +6,13 @@ app = Flask(__name__)
 @app.route("/", methods=["GET", "POST"])
 def guess_the_number():
     if request.method == "GET":
-        return render_template("start.html", min = 0, max = 1001)
+        return render_template("start.html", min = 0, max = 1001, attempts = 0)
     else:
-        min_number = int(request.form.get["min"])
-        max_number = int(request.form.get["max"])
-        user_answer = request.form.get["user_answer"]
-        guess = int(request.form.get["guess", 500])
+        min_number = int(request.form.get("min", "0"))
+        max_number = int(request.form.get("max", "1001"))
+        user_answer = request.form.get("user_answer", "")
+        guess = int(request.form.get("guess", 500))
+        attempts = int(request.form.get("attempts", "0")) + 1
 
         if user_answer == "too big":
             max_number = guess
@@ -22,8 +23,8 @@ def guess_the_number():
 
         guess = (max_number - min_number) // 2 + min_number
 
-        return render_template("main.html", guess=guess, min=min_number, max=max_number)
+        return render_template("program.html", guess=guess, min=min_number, max=max_number, attempts=attempts)
 
 
-if __name__ == '__main__':
-    app.run()
+if __name__ == "__main__":
+    app.run(debug=True, port=5010)
